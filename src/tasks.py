9da8eb6a7d76f61cd24b38c61fe729c5bf956943
Sebastian Wezel <swezel@data-modul.com>
2017-03-29 16:05:16 +0200

    tasks: added task list class
    Signed-off-by: Sebastian Wezel <swezel@data-modul.com>

---
+++ src/tasks.py
@@ -0,0 +1,19 @@
+class Tasks:
+    Play = 1
+    Stop = 2
+    Next = 3
+    Prev = 4
+    Toggle = 5
+    Pause = 6
+    MetaData = 7
+    Daemon = 8
+    Tmux = 9
+    VolInc = 10
+    VolDec = 11
+    SecFwd = 12
+    SecBwd = 13
+    Seek = 14
+    Lyric = 15
+    NONE = -1
+
+
