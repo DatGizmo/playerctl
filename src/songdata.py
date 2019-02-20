6083dc49605d8c5e3ba9eb8be9accebb7aed1ba5
Sebastian Wezel <Sebastian.Wezel@harman.com>
2019-02-20 13:05:37 +0100

    Added lyricsmaster as additional fetcher. As this fetcher is storing data locally, we now search there first. The file are store case senitive, but this could result in case missmatches, so the files are renamed/moved to lower case verison. This is not yet finished
    Signed-off-by: Sebastian Wezel <Sebastian.Wezel@harman.com>

--- src/songdata.py
+++ src/songdata.py
@@ -1,8 +1,13 @@
 import requests
 from datetime import datetime
 from lyricsmaster import Genius
+from lyricsmaster import LyricWiki
 from os.path import expanduser
 from os import path
+from os import makedirs
+from os import walk
+from os import removedirs
+from shutil import move
 
 class SongData(object):
     def __init__(self, artist, album, track, title, fn, length, elapsed):
@@ -14,6 +19,10 @@ class SongData(object):
         self.filename = fn
         self.length = length
         self.elapsed = elapsed
+        self.lyricroot = path.join(expanduser("~"), "Documents/LyricsMaster/")
+        self.artistroot = ''
+        self.albumroot = ''
+        self.lyricspath = ''
 
     def __str__(self):
         retval = ''
@@ -40,45 +49,129 @@ class SongData(object):
 
     def toString(self):
         return self.__str__()
-    
+
+    def setPaths(self):
+        self.artistroot = path.join(self.lyricroot, self.artist.lower()).replace(' ', '-')
+        self.albumroot = path.join(self.artistroot, self.album.lower()).replace(' ', '-')
+        self.lyricspath = path.join(self.albumroot, self.title.lower() + '.txt').replace(' ', '-')
+
+    def checkFileExists(self, create=False):
+        exists = False
+        if(not self.album or not self.artist):
+            return self.searchFolder()
+        self.setPaths()
+        print(self.artistroot)
+        if path.exists(self.artistroot):
+            print(self.albumroot)
+            if path.exists(self.albumroot):
+                print(self.lyricspath)
+                if path.exists(self.lyricspath):
+                    print("True")
+                    exists = True
+        if(False == exists and True == create):
+            makedirs(path.join(self.lyricroot, self.artist.lower(), self.album.lower()).replace(' ', '-'))
+            exists = True
+        if(not exists and self.searchFolder()):
+            exists = True
+        return exists
+
+    def fetchFromProvider(self, provider):
+        if provider:
+            data = provider.get_lyrics(self.artist)
+            data.save()
+            # Move all to lower case version
+            for root, dirs, files in walk(self.lyricroot):
+                for dr in dirs:
+                    if self.artist.lower().replace(' ', '-') in dr.lower():
+                        artsrc = dr
+                        artdest = dr.lower()
+                        if(not path.exists(path.join(root, artdest))):
+                            move(path.join(root, artsrc), path.join(root, artdest))
+                            artsrc = artdest
+                        for root2, dirs2, files2 in walk(path.join(root, artsrc)):
+                            for drr in dirs2:
+                                albumsrc = drr
+                                albumdest = drr.lower()
+                                if(not path.exists(path.join(root, artdest, albumdest))):
+                                    move(path.join(root2,  albumsrc), path.join(root, artdest, albumdest))
+                                    albumsrc = albumdest
+                                    print(albumsrc)
+                                for root3, dirs3, files3 in walk(path.join(root, artsrc, albumsrc)):
+                                    for fp in files3:
+                                        move(path.join(root3, fp), path.join(root, artdest, albumdest, fp.lower()))
+                                rmalbump = path.join(root, dr, drr)
+                                if(path.exists(rmalbump)):
+                                    try:
+                                        removedirs(rmalbump)
+                                    except OSError:
+                                        pass
+                            rmartp = path.join(root, dr)
+                            if(path.exists(rmartp)):
+                                try:
+                                    removedirs(path.join(root, dr)) 
+                                except OSError:
+                                    pass
+
     def fetchLyric(self):
-        provider = Genius()
-        song = provider.get_lyrics(self.artist)
-        data.save()
-        getLyricFromFile()
+        print("Fetching artists lyrics with lyricsmaster using Genius")
+        self.fetchFromProvider(Genius())
+        if(not self.checkFileExists()):
+            self.getLyricFromFile()
+#           print("Fetching artists lyrics with lyricsmaster using LyricWiki")I
+#            self.fetchFromProvider(LyricWiki())
+#            if(not self.checkFileExists()):
+#               return 
+
 
     def getLyricFromFile(self):
-        dirs = path.join(expanduser("~"), "Documents/LyricsMaster/", self.artist).replace(' ', '-')
-        if path.exists(dirs):
-            albump = path.join(dirs, self.album).replace(' ', '-')
-            if path.exists(albump):
-                lyricp = path.join(albump, self.title + '.txt').replace(' ', '-')
-                print(lyricp)
-                if path.exists(lyricp):
-                    print("Blub")
-                    fp = open(lyricp)
-                    self.lyric = fp.read()
-                    fp.close()
+        fp = open(self.lyricspath)
+        self.lyric = fp.read()
+        fp.close()
 
     def fetchFromMIP(self): 
+        print("Fetching with MIP")
         payload = {'artist': self.artist, 'title': self.title}
         r = requests.get('https://makeitpersonal.co/lyrics', params = payload)
-        self.lyric = r.text
-        #save to file
+        if("Sorry, We don't have lyrics for this song yet" not in r.text):
+            self.lyric = r.text
+            if(self.album):
+                self.checkFileExists(True)
+                fp = open(self.lyricspath, 'w+')
+                fp.write(self.lyric)
+                fp.close()
+
+    def searchFolder(self):
+        result = []
+        retval = False
+        fileName = self.title.replace(' ', '-') + ".txt"
+        for root, dirs, files in walk(self.lyricroot):
+            if fileName.lower() in files:
+                result.append(path.join(root, fileName.lower()))
+        if len(result) >= 1:
+            self.lyricspath = result[0]
+            self.getLyricFromFile()
+            retval = True
+        else:
+            print("Could not find file with lyrics for \"%s\"" % self.title)
+        return retval
 
     def getLyric(self):
-        if not self.lyric:
-            self.getLyricFromFile()
-        if not self.lyric:
+        if self.title and not self.artist or not self.album:
+            self.searchFolder()
+        elif(self.checkFileExists()):
+            if not self.lyric:
+                self.getLyricFromFile()
+        if self.title and self.artist and not self.lyric:
             self.fetchFromMIP()
-        if not self.lyric:
+        if self.artist and not self.lyric:
             self.fetchLyric()
 
-        if self.lyric[0] == '\n':
-            self.lyric = self.lyric[1:len(self.lyric)]
-        if self.lyric[len(self.lyric)-1] == '\n':
-            self.lyric = self.lyric[0:len(self.lyric)-1]
+        if self.lyric:
+            if self.lyric[1] == '\n':
+                self.lyric = self.lyric[2:len(self.lyric)]
+            if self.lyric[len(self.lyric)-1] == '\n':
+                self.lyric = self.lyric[0:len(self.lyric)-1]
 
-        print(self)
-        print(self.lyric)
+            print(self)
+            print(self.lyric)
 
